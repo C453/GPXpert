03545958f84bc6074d101d263fd56460a9b1361d
C453 <Caseman912@me.com>
2024-04-19 22:51:08 -0700

    fix map

--- src/app/components/Map.tsx
+++ src/app/components/Map.tsx
@@ -1,26 +1,26 @@
-"use client";
+'use client';
 
-import {MapContainer, Polyline, TileLayer, useMap} from "react-leaflet";
-import type {LatLngTuple} from "leaflet";
+import { MapContainer, Polyline, TileLayer, useMap } from 'react-leaflet';
+import type { LatLngTuple } from 'leaflet';
 
-import "leaflet/dist/leaflet";
-import {useEffect} from "react";
+import 'leaflet/dist/leaflet';
+import { useEffect } from 'react';
 
 type MapProps = Readonly<{
   positions: LatLngTuple[];
 }>;
 
-export default function Map({positions}: MapProps) {
+export default function Map({ positions }: MapProps) {
   return (
     <MapContainer
       center={positions[0]}
       zoom={15}
       scrollWheelZoom={true}
-      style={{flexGrow: 1}}
+      style={{ flexGrow: 1 }}
     >
-      <TileLayer url="https://tiles.stadiamaps.com/tiles/outdoors/{z}/{x}/{y}{r}.png" />
+      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
       <Polyline
-        pathOptions={{fillColor: "red", color: "blue"}}
+        pathOptions={{ fillColor: 'red', color: 'blue' }}
         positions={positions}
       />
     </MapContainer>
